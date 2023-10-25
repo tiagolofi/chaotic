eddafa2e519c226359f7973f0e8639a35dccad52
tiagolofi <jejeco75@gmail.com>
2023-10-24 22:50:02 -0300

    reorder columns

--- app.py
+++ app.py
@@ -22,26 +22,6 @@ with col1:
 
     st.image(Image.open(d.format_image(data1['chaotic_hash'])))
 
-    st.write(data1['stats'])
-
-    col1_1, col1_2 = st.columns(2)
-
-    with col1_1:
-
-        bg = st.selectbox('BattleGear (P1)', d.get_names_bg())
-
-        data_bg = d.get_card(name = bg)
-
-        st.image(Image.open(d.format_image(data_bg['chaotic_hash'])))
-
-    with col1_2:
-
-        mugic = st.selectbox('Mugic (P1)', d.get_names_mugix())
-
-        data_mg = d.get_card(name = mugic)
-
-        st.image(Image.open(d.format_image(data_mg['chaotic_hash'])))
-
 with col2:
 
     select_local = st.button('Choose Battle Local')
@@ -70,22 +50,44 @@ with col3:
 
     st.image(Image.open(d.format_image(data2['chaotic_hash'])))
 
+col1_1, col1_2, col1_3, col2_1, col2_2, col2_3 = st.columns(6)
+
+with col1_1:
+
     st.write(data1['stats'])
 
-    col2_1, col2_2 = st.columns(2)
+with col1_2:
 
-    with col2_1:
+    bg = st.selectbox('BattleGear (P1)', d.get_names_bg())
 
-        bg2 = st.selectbox('BattleGear (P2)', d.get_names_bg())
+    data_bg = d.get_card(name = bg)
 
-        data_bg2 = d.get_card(name = bg2)
+    st.image(Image.open(d.format_image(data_bg['chaotic_hash'])))
 
-        st.image(Image.open(d.format_image(data_bg2['chaotic_hash'])))
+with col1_3:
 
-    with col2_2:
+    mugic = st.selectbox('Mugic (P1)', d.get_names_mugix())
 
-        mugic2 = st.selectbox('Mugic (P2)', d.get_names_mugix())
+    data_mg = d.get_card(name = mugic)
 
-        data_mg2 = d.get_card(name = mugic2)
+    st.image(Image.open(d.format_image(data_mg['chaotic_hash'])))
 
-        st.image(Image.open(d.format_image(data_mg2['chaotic_hash'])))
+with col2_1:
+
+    st.write(data2['stats'])
+
+with col2_2:
+
+    bg2 = st.selectbox('BattleGear (P2)', d.get_names_bg())
+
+    data_bg2 = d.get_card(name = bg2)
+
+    st.image(Image.open(d.format_image(data_bg2['chaotic_hash'])))
+
+with col2_3:
+
+    mugic2 = st.selectbox('Mugic (P2)', d.get_names_mugix())
+
+    data_mg2 = d.get_card(name = mugic2)
+
+    st.image(Image.open(d.format_image(data_mg2['chaotic_hash'])))
