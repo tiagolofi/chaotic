8db36d781500e843a13fbf5479ddb7df0ea43b21
tiagolofi <jejeco75@gmail.com>
2023-10-24 22:15:56 -0300

    cols

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
 
-    creature = st.selectbox(d.get_names_creatures())
+    creature = st.selectbox('Creature (Player 1)', d.get_names_creatures())
+
+    data1 = d.get_card(name = creature)
+
+    st.image(Image.open(d.format_image(data1['chaotic_hash'])))
 
 with col2:
 
@@ -22,3 +26,12 @@ with col2:
                 d.location_random()
             )
         )        
+
+with col3:
+
+    creature = st.selectbox('Creature (Player 2)', d.get_names_creatures())
+
+    data2 = d.get_card(name = creature)
+
+    t.image(Image.open(d.format_image(data2['chaotic_hash'])))
+    
