a9f502b92327de0b0e593a368ce08eedc2cb90d7
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-10-26 14:30:44 -0300

    teste

--- app.py
+++ app.py
@@ -117,8 +117,8 @@ with col1_3:
 
     if action:
 
-        data1['stats'][var] = data1['stats'].get(var) += value_credit
-        data1['stats'][var] = data1['stats'].get(var) -= value_debit
+        data1['stats'].get(var) += value_credit
+        data1['stats'].get(var) -= value_debit
 
     # st.write(data1['stats'])
 
@@ -140,8 +140,8 @@ with col2_1:
 
     if action2:
 
-        data2['stats'][var] = data2['stats'].get(var) += value_credit
-        data2['stats'][var] = data2['stats'].get(var) -= value_debit
+        data2['stats'].get(var) += value_credit
+        data2['stats'].get(var) -= value_debit
 
 with col2_2:
 
