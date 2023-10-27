7966fd8541fbddbd12d8d815737c6e903c5ae0b5
tiagolofi <jejeco75@gmail.com>
2023-10-26 22:44:54 -0300

    keys

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ with col1_3:
 
     with st.expander('Stats (P1)'):
 
-        data1_ed = st.data_editor(pandas.DataFrame(data1).filter(['stats']))
+        data1_ed = st.data_editor(pandas.DataFrame(data1).filter(['stats']), key = 'd1')
 
     for k, v in zip(data1_ed.index, data1_ed['stats']):
 
@@ -118,7 +118,7 @@ with col2_1:
 
     with st.expander('Stats (P2)'):
 
-        data2_ed = st.data_editor(pandas.DataFrame(data2).filter(['stats']))
+        data2_ed = st.data_editor(pandas.DataFrame(data2).filter(['stats'])), key = 'd2')
 
     for k2, v2 in zip(data2_ed.index, data2_ed['stats']):
         
